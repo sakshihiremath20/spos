b8fe820f32d05e669696ed5abc160cf1a871166d
sakshihiremath20 <sakshih.200@gmail.com>
2026-02-06 12:14:37 +0530

    Convert JavaScript FIFO example to Java

--- fifo.java
+++ fifo.java
@@ -1,35 +1,33 @@
-<script>
+import java.util.LinkedList;
+import java.util.Queue;
 
-// JavaScript program to demonstrate
-// working of FIFO
-// using Queue interface in Java
+public class QueueExample {
+    public static void main(String[] args)
+    {
+        Queue<Integer> q = new LinkedList<>();
 
-let q = [];
-// Adds elements {0, 1, 2, 3, 4} to queue
-for (let i = 0; i < 5; i++)
-    q.push(i);
+        // Adds elements {0, 1, 2, 3, 4} to queue
+        for (int i = 0; i < 5; i++)
+            q.add(i);
 
-// Display contents of the queue.
-document.write("Elements of queue-[" + q.join(", ")+"]<br>");
+        // Display contents of the queue.
+        System.out.println("Elements of queue-" + q);
 
-// To remove the head of queue.
-// In this the oldest element '0' will be removed
-let removedele = q.shift();
-document.write("removed element-" + removedele+"<br>");
+        // To remove the head of queue.
+        // In this the oldest element '0' will be removed
+        int removedele = q.remove();
+        System.out.println("removed element-" + removedele);
 
-document.write("["+q.join(", ")+"]<br>");
+        System.out.println(q);
 
-// To view the head of queue
-let head = q[0];
-document.write("head of queue-" + head+"<br>");
+        // To view the head of queue
+        int head = q.peek();
+        System.out.println("head of queue-" + head);
 
-// Rest all methods of collection interface,
-// Like size and contains can be used with this
-// implementation.
-let size = q.length;
-document.write("Size of queue-" + size+"<br>");
-
-
-// This code is contributed by avanitrachhadiya2155
-
-</script>
+        // Rest all methods of collection interface,
+        // Like size and contains can be used with this
+        // implementation.
+        int size = q.size();
+        System.out.println("Size of queue-" + size);
+    }
+}
